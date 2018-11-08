10f640fd2b96b68228d77ed94032469436ef2070
Ondrej Glasnak <glasnak.o@gmail.com>
2018-11-08 21:29:58 +0100

    Updates

---
+++ precache-manifest.6a5b0793f28f301a37d5b9625048d21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82bac5355f1115587bc1c10ee1663199",
+    "url": "/todoge/static/media/doge.82bac535.svg"
+  },
+  {
+    "revision": "dccbffbbd06641d7ff3b",
+    "url": "/todoge/static/js/runtime~main.dccbffbb.js"
+  },
+  {
+    "revision": "9299d19d3abdfbaa6c0f",
+    "url": "/todoge/static/js/main.9299d19d.chunk.js"
+  },
+  {
+    "revision": "8c0859d6a58d61cc3a39",
+    "url": "/todoge/static/js/1.8c0859d6.chunk.js"
+  },
+  {
+    "revision": "9299d19d3abdfbaa6c0f",
+    "url": "/todoge/static/css/main.d59f6585.chunk.css"
+  },
+  {
+    "revision": "9a247de1dc771c65ce03e2b57d945172",
+    "url": "/todoge/index.html"
+  }
+];
