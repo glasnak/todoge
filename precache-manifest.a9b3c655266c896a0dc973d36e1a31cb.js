471387fd5a1f281b1ee85a83a6657f99c505935b
Ondrej Glasnak <glasnak.o@gmail.com>
2019-06-17 21:20:31 +0200

    Updates

---
+++ precache-manifest.a9b3c655266c896a0dc973d36e1a31cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e207f01196fa85b6075c06c1144144",
+    "url": "/todoge/index.html"
+  },
+  {
+    "revision": "e1a73db6a3b19e21acc3",
+    "url": "/todoge/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/todoge/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "e1a73db6a3b19e21acc3",
+    "url": "/todoge/static/js/main.c147e5ae.chunk.js"
+  },
+  {
+    "revision": "32446ef581d5c7ebb727",
+    "url": "/todoge/static/js/runtime~main.d3c21820.js"
+  },
+  {
+    "revision": "82bac5355f1115587bc1c10ee1663199",
+    "url": "/todoge/static/media/doge.82bac535.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todoge/precache-manifest.6a5b0793f28f301a37d5b9625048d21e.js"
+  "/todoge/precache-manifest.a9b3c655266c896a0dc973d36e1a31cb.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/todoge/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todoge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
